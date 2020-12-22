c62d835ffdd9fab86a77413b2df2910b5450a0b1
João Almeida <joaoalmeida11@ua.pt>
2020-12-22 22:55:34 +0000

    At3  passing codecov

--- src/components/Homepage/ShowLeague.js
+++ src/components/Homepage/ShowLeague.js
@@ -1,10 +1,6 @@
-// local images have to be imported
-// import  
-// import ImageLeague from '../../images/leagues';
-
 // o scr da imagem n funciona pq n sei onde ele começa... verificar como é que se comporta com uma API
 export default function ShowLeague(props){
-    let altImage='League of '+props.league.name;
+    const altImage='League of '+props.league.name;
 
     return(
         <article className="col-12 col-md-6 col-lg-4 text-center">
