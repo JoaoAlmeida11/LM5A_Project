1aa128d0b104b0cab1a95e18d0ab2dbda4b4e1ab
Joao Almeida <jopecoal@gmail.com>
2020-12-22 16:05:47 +0000

    Revert "Revert "NavCostum base""
    This reverts commit 203bdfbcb3e1848844cd5353062764d4b7746062.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 import HomePage from "./components/Homepage/HomePage"
@@ -10,21 +9,26 @@ import League from "./components/League/League"
 import Club from './components/Club/Club';
 import Player from './components/Player/Player';
 import NavCostum from "./components/Layout/NavCostum";
+import Ideal11 from "./components/Ideal11/Ideal11";
+
 
 export default function App() {
   return (
     <Router>
-        <div>
+        <header>
           <NavCostum />
-        </div>
+        
         <Switch>
+          {/* o deployment está a mostrar lm5a_project mas o NavBar apenas mostra o path
+              é preciso ver se é necessário adicionar o nome do projeto ao Route de todos
+          */}
           <Route exact path="/" component={HomePage}></Route>
           <Route path="/league" component={League}></Route>
           <Route path="/club" component={Club}></Route>
           <Route path="/player" component={Player}></Route>
-          <Route path="/ideal11" component={Player}></Route>
+          <Route path="/ideal11" component={Ideal11}></Route>
         </Switch>
-
+</header>
     </Router>
   );
 }
--- src/components/Homepage/ShowLeague.js
+++ src/components/Homepage/ShowLeague.js
@@ -1,12 +1,12 @@
 // local images have to be imported
 
-export default function ShowLeague(props){
+export default function ShowLeague(){
     return(
         <article className="col-12 col-md-6 col-lg-4">
             <figure>
-                <img src={props.image} />
+                {/* <img src={props.image} /> */}
             </figure>
-            <p className="text-center">{props.name}</p>
+            <p className="text-center"></p>
             <button className="homepageButtonLeague"></button>
         </article>
     )
