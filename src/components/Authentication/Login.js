7cee78f50e83e8a0558192162eea269ef79b4591
Bruno Paquete <bruno.paquete@ua.pt>
2020-12-23 02:03:21 +0000

    LoginForm

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,9 +1,33 @@
 
+import {Form, Button} from 'react-bootstrap';
+
 
 export default function Login(){
     return(
     <div>
-        <p>Olá</p>
+       <Form>
+                <h3>Sign In</h3>
+
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control value="email"  placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control value="password"  placeholder="Enter password" />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Control custom={true} className="custom-control custom-checkbox">
+                        <Form.Check type="checkbox" id="customCheck1" />
+                        <Form.Label className="custom-control-Form.Label" htmlFor="customCheck1">Remember me</Form.Label>
+                    </Form.Control>
+                </Form.Group>
+
+                <Button value="submit" variant="primary">Submit</Button>
+                <Form.Text className="forgot-password text-right">Forgot</Form.Text>
+        </Form>
     </div>
     )
 }
