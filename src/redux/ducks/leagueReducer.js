341d7cd6eaa53ef6f633f8832854dc8be32f84ad
João Almeida <joaoalmeida11@ua.pt>
2020-12-26 12:09:52 +0000

    merge

--- src/redux/ducks/leagueReducer.js
+++ src/redux/ducks/leagueReducer.js
@@ -1,69 +1,24 @@
 
-const INCREMENT = 'INCREMENT'
-function incrementOriginal() {
-  return { type: INCREMENT }
-}
+const { actions, reducer } = counterSlice
+const { increment, decrement } = actions
 
-// const increment = createAction('INCREMENT')
-// const decrement = createAction('DECREMENT')
+  const counterSlice = createSlice({
+    name: 'counter',
+    initialState: 0,
+    reducers: {
+      increment: state => state + 1,
+      decrement: state => state - 1
+    }
+  })
 
-
-// function counter(state = 0, action) {
-//   switch (action.type) {
-//     case increment.type:
-//       return state + 1
-//     case decrement.type:
-//       return state - 1
-//     default:
-//       return state
-//   }
-// }
-
-// const counter = createReducer(0, {
-//     [increment.type]: state => state + 1,
-//     [decrement.type]: state => state - 1
-//   })
-// = to below since toString() is overridden to .type
-// const counter = createReducer(0, {
-//     [increment]: state => state + 1,
-//     [decrement]: state => state - 1
-//   })
-
-// That's where the createSlice function comes in. It allows us to provide an object with the reducer functions, and it will automatically generate the action type strings and action creator functions based on the names of the reducers we listed.
-
-// const counterSlice = createSlice({
-//     name: 'counter',
-//     initialState: 0,
-//     reducers: {
-//       increment: state => state + 1,
-//       decrement: state => state - 1
-//     }
-//   })
-  
-//   const store = configureStore({
-//     reducer: counterSlice.reducer
-//   })
-
-  
-
-
-  const store = Redux.createStore(counter)
-// needs to specify to start devtools
-
-// const store = configureStore({
-//     reducer: counter
-// })
-// automatically starts dev tools
-
-document.getElementById('increment').addEventListener('click', () => {
-  store.dispatch(increment())
+const store = configureStore({
+    reducer: reducer
 })
 
-// document.getElementById('increment').addEventListener('click', () => {
-//     store.dispatch(counterSlice.actions.increment())
-//   })
+// async
+document.getElementById('increment').addEventListener('click', () => {
+    store.dispatch(actions.increment())
+  })
 
 
-//   to simplify
-// const { actions, reducer } = counterSlice
-// const { increment, decrement } = actions
+
