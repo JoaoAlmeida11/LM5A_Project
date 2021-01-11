ef91f5b3923d8e78ff6d8eb9bad5d8a6333e9402
João Almeida <joaoalmeida11@ua.pt>
2021-01-11 15:11:01 +0000

    leagueList request completed; still need to update Homepage

--- src/redux/ducks/leagueSlice.js
+++ src/redux/ducks/leagueSlice.js
@@ -6,130 +6,107 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 export const leagueIdEntity = new schema.Entity('leagueId');
 export const leagueLogoEntity = new schema.Entity('leagueLogo');
 export const leagueNameEntity = new schema.Entity('leagueName');
+// TODO: se schema of seasons
 export const leagueSeasonsEntity = new schema.Entity('leagueSeasons');
 
-// TODO: se if schema can be setted to primitive type
 export const leagueEntity = new schema.Entity('leagues', {
-	id: [leagueIdEntity],
-	logo: [leagueLogoEntity],
-	name: [leagueNameEntity],
+	id: Number,
+	logo: String,
+	name: String,
 	seasons: [leagueSeasonsEntity],
 });
+// export const leagueEntity = new schema.Entity('leagues');
 export const leagueListEntity = new schema.Entity('leagueList', {
 	list: [leagueEntity],
 });
-
+export const fetchData = url => {
+	const axios = require('axios').default;
+	return axios
+		.get(url)
+		.then(function (response) {
+			// console.log(response.data);
+			return response.data;
+		})
+		.catch(function (err) {
+			console.log(err);
+			return err;
+		});
+};
+export const getAllData = urls => {
+	return Promise.all(urls.map(fetchData));
+};
 export const fetchLeaguesAll = createAsyncThunk(
 	'leagues/requestStatus',
 	async () => {
 		//pass an obj with id (only allows one)
 		const axios = require('axios').default;
-		// the API calls can't be done with Promise.All do to needing the ids from the first call
-
-		const firstCall = new Promise((fulfilled, reject) => {
-			const response = axios
-				.get(`https://api.statorium.com/api/v1/leagues/?apikey=${API_KEY}`)
-				.then(res => {
-					console.table(res.data);
-					const leagueSet = new Set();
-					for (let i in res.data.leagues) {
-						// the key corresponds to the position on the array from the first call and its used to match each league with its image
-						leagueSet.add(res.data.leagues[i].id);
-					}
-
-					fulfilled(leagueSet);
-
-					// return res.data;
-				})
-				.catch(err => {
-					console.log('err');
-					reject(err);
-					return err;
+		const response = await axios
+			.get(`https://api.statorium.com/api/v1/leagues/?apikey=${API_KEY}`)
+			.then(res => {
+				// console.log(res.data);
+				const leagueSet = new Set();
+				for (let i in res.data.leagues) {
+					// the key corresponds to the position on the array from the first call and its used to match each league with its image
+					leagueSet.add(res.data.leagues[i].id);
+				}
+				const ids = [...leagueSet];
+				return ids;
+			})
+			.then(res => {
+				// console.log('SecondThen');
+				// console.log(res);
+				const urls = [];
+				res.forEach(value => {
+					urls.push(
+						`https://api.statorium.com/api/v1/leagues/${value}/?apikey=${API_KEY}`
+					);
 				});
-		});
-		const response = firstCall.then(leagueSet => {
-			console.log('YAA');
-			console.log(leagueSet);
-			const urls = new Set();
-			leagueSet.forEach(value => {
-				urls.add(
-					`https://api.statorium.com/api/v1/leagues/${value}/?apikey=f74d57716d0cb84d043c77b384885aeb`
-				);
+				return getAllData(urls)
+					.then(res => {
+						console.log(res);
+						return res;
+					})
+					.catch(err => {
+						console.log(err);
+						return err;
+					});
+			})
+			.catch(err => {
+				console.log(err);
+				return err;
 			});
-			console.log(urls);
-			// let a = 0;
-			// for (let item in leagueSet) {
-			// 	console.log('item');
-			// 	console.log(item);
-			// 	// if (a === 0) {
-			// 	// 	const url1 = axios.get(
-			// 	// 		`https://api.statorium.com/api/v1/leagues/${item}/?apikey=f74d57716d0cb84d043c77b384885aeb`
-			// 	// 	);
-			// 	// 	a += 1;
-			// 	// } else {
-			// 	// 	const url2 = axios.get(
-			// 	// 		`https://api.statorium.com/api/v1/leagues/${item}/?apikey=f74d57716d0cb84d043c77b384885aeb`
-			// 	// 	);
-			// 	// }
-			// }
+		// additional call for image base on id
+		// https://api.statorium.com/api/v1/leagues/1/?apikey=123_test_token_123
 
-			// console.log(url1);
-			// console.log(url2);
-			// const url1 = axios.get(
-			// 	`https://api.statorium.com/api/v1/leagues/${value}/?apikey=f74d57716d0cb84d043c77b384885aeb`
-			// );
+		// const responseAll = fetchLeaguesEach(response);
+		console.log('ResonseMainThunk');
+		console.log(response);
+		// keeps only the leagues
+		const responseList = [];
+		for (let i in response) {
+			responseList.push(response[i].league);
+		}
+		console.log('responseList');
+		console.log(responseList);
+		// keeps only the last season
+		for (let i in responseList) {
+			responseList[i].seasons = [
+				responseList[i].seasons[responseList[i].seasons.length - 1],
+			];
+		}
+		console.log('seasons');
+		console.log(responseList);
 
-			// leagueSet.forEach(value => {
-
-			// }
-
-			// leagueSet.forEach(value => {
-			// 	const response = axios
-			// 		.get(
-			// 			`https://api.statorium.com/api/v1/leagues/${value}/?apikey=f74d57716d0cb84d043c77b384885aeb`
-			// 		)
-			// 		.then(res => {
-			// 			// console.log(res.data);
-			// 			return res.data;
-			// 		})
-			// 		.catch(err => {
-			// 			console.log(err);
-			// 			return err;
-			// 		});
-			// });
-			console.log(response);
-			return response;
-		});
-		const normalized = normalize(response.leagues, [leagueListEntity]);
+		const normalized = normalize(responseList, [leagueListEntity]);
+		// console.log(normalized.entities);
 		return normalized.entities;
-		// const response =  new Promise.all()
-
-		//  otherCalls = new Promise.all(
-
-		//  )
-		//  responseImage = leagueSet.forEach(value => {
-		// 	return axios
-		// 		.get(
-		// 			`https://api.statorium.com/api/v1/leagues/${value}/?apikey=f74d57716d0cb84d043c77b384885aeb`
-		// 		)
-		// 		.then(res => {
-		// 			console.log(res.data);
-		// 			return;
-		// 		})
-		// 		.catch(err => {
-		// 			console.log(err);
-		// 			reject(err)
-		// 			return err;
-		// 		});
-		// });
-		// console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-		// console.log(responseImage);
-
-		// }
-		// const normalized = normalize(response.leagues, [leagueListEntity]);
-		// return normalized.entities;
 	}
 );
+
+// const normalized = normalize(response.leagues, [leagueListEntity]);
+// return normalized.entities;
+// 	}
+// );
 
 const leagueSlice = createSlice({
 	name: 'leagues',
@@ -144,12 +121,26 @@ const leagueSlice = createSlice({
 		},
 		[fetchLeaguesAll.fulfilled]: (state, { payload }) => {
 			state.loading = 'success';
+			// ! its only receiving one
 
-			state.leagueList = payload.leagueList;
+			console.log('payload');
+			console.log(payload);
+			state.leagueList = payload;
 		},
 		[fetchLeaguesAll.rejected]: state => {
 			state.loading = 'failed';
 		},
+		// [fetchLeaguesEach.pending]: state => {
+		// 	state.loading = 'pending';
+		// },
+		// [fetchLeaguesEach.fulfilled]: (state, { payload }) => {
+		// 	state.loading = 'success';
+
+		// 	state.leagueList = payload.leagueList;
+		// },
+		// [fetchLeaguesEach.rejected]: state => {
+		// 	state.loading = 'failed';
+		// },
 	},
 });
 
