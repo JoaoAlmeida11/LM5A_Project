640fab608f1e82db70518f00a2e293d481c7e180
João Almeida <joaoalmeida11@ua.pt>
2021-01-13 15:37:32 +0000

    APIcalls working; response not waiting for calls

--- src/redux/ducks/clubsSlice.js
+++ src/redux/ducks/clubsSlice.js
@@ -37,33 +37,24 @@ export const fetchPlayersId = res => {
 };
 export const fetchEachClub = res => {
 	console.log('fetchEachClub - Here');
-	console.log(res);
+	// console.log(sres);
 	const participants = res.season.participants;
-	console.log('participants');
-	console.log(participants);
-
+	// console.log('participants');
+	// console.log(participants);
+	const seasonId = res.season.seasonID;
 	let urls = [];
 	for (let i in participants) {
-		console.log(i);
-		// urls.push()
+		urls.push(
+			`https://api.statorium.com/api/v1/teams/${participants[i].participantID}/?season_id=${seasonId}&apikey=${API_KEY}`
+		);
 	}
+	// console.log(urls);
 
-	return Promise.resolve(true);
-	// return Promise.resolve(fetchAllDataConcurrently(url));
+	return Promise.resolve(fetchAllDataConcurrently(urls));
 };
 // ** Promise Chaining
-// function myPromiseFunction(needsLeagueList) {
-// 	//Change the resolved value to take a different path
-// 	return Promise.resolve(needsLeagueList);
-// }
 export const conditionalChaining = ({ needsLeagueList, leagueId }) => {
-	// console.log('needsLeagueList');
-	// console.log(needsLeagueList);
-	// console.log('leagueId');
-	// console.log(leagueId);
-
 	if (needsLeagueList) {
-		// console.log('about to do leagueId API call');
 		const url = `https://api.statorium.com/api/v1/leagues/${leagueId}/?apikey=${API_KEY}`;
 		return fetchAllData(url);
 	} else {
@@ -93,7 +84,7 @@ export const fetchClubs = createAsyncThunk(
 		// code improvement
 		let needsLeagueList = leagueListArray.length !== 0 ? false : true;
 
-		const response = conditionalChaining({ needsLeagueList, leagueId }) //**working */
+		const response = await conditionalChaining({ needsLeagueList, leagueId }) //**working */
 			.then(res => {
 				console.log('All done! - Success');
 				// console.log(res);
@@ -103,86 +94,22 @@ export const fetchClubs = createAsyncThunk(
 				return fetchPlayersId(res);
 			})
 			.then(res => {
+				// const lastInfo = fetchEachClub(res);
+				// console.log(lastInfo)
 				return fetchEachClub(res);
+			})
+			.then(res => {
+				console.log('EndOfLine');
+				console.log(res);
 			})
 			.catch(error => {
 				console.log(error);
 				return error;
 			});
-
-		// if (leagueListArray.length !== 0) {
-		// 	// get the existing ids of the leagues in the store
-		// 	let storeLeagueIds = [];
-		// 	for (let i in state.league.leagueList) {
-		// 		storeLeagueIds.push(state.league.leagueList[i].seasons[0].seasonID);
-		// 	}
-
-		// 	// ** fetch the id of the clubs of a league (participantID)
-		// 	seasonId = storeLeagueIds[storeLeagueIds.length - 1]; //retrieve the last position of the seasons array
-		// 	console.log(seasonId);
-		// 	// const axios = require('axios').default;
-		// 	url = `https://api.statorium.com/api/v1/seasons/${seasonId}/?apikey=${API_KEY}`;
-		// 	response = fetchAllData(url)
-		// 		.then(res => {
-		// 			// ** get the ids of the clubs as participants
-		// 			console.log(res);
-		// 			console.log('YAA');
-		// 			return res.season;
-		// 		})
-		// 		.then(res => {
-		// 			// ** get the data of each club
-		// 			// needs seasonID and participantID
-		// 			// needs the urls in an array
-		// 			console.log(res);
-		// 			response = fetchAllDataConcurrently(res);
-		// 			console.log('fetchAllDataConcurrently');
-		// 			console.log(response);
-		// 		})
-		// 		.catch(err => {
-		// 			console.log('NOO');
-
-		// 			console.log(err);
-		// 			return err;
-		// 		});
-		// } else {
-		// 	// call API to get the last seasonID of each league
-		// 	console.log('Store is empty!');
-		// 	let url = `https://api.statorium.com/api/v1/leagues/${leagueId}/?apikey=${API_KEY}`;
-		// 	response = fetchAllData(url)
-		// 		.then(res => {
-		// 			console.log('InMainThunk');
-		// 			console.log(res);
-		// 			return res;
-		// 		})
-		// 		.then(res => {
-		// 			// retrive the seasonId
-		// 			seasonId = res.league.seasons[res.league.seasons.length - 1].seasonID;
-		// 			console.log('seasonId');
-		// 			console.log(seasonId);
-		// 			url = `https://api.statorium.com/api/v1/seasons/${seasonId}/?apikey=${API_KEY}`;
-		// 			// ** get the ids of the clubs as participants
-		// 			response = fetchAllData(url);
-		// 			console.log('PlayersId');
-		// 			console.log(response);
-		// 			// extractSeasonId(r)
-		// 		})
-		// 		.then(res => {
-		// 			// ** get the data of each club
-		// 			// needs seasonID and participantID
-		// 			// needs the urls in an array
-
-		// 			// response = fetchAllDataConcurrently(res);
-		// 			console.log('fetchAllDataConcurrently');
-		// 			console.log(res);
-		// 			// console.log(response);
-		// 		})
-		// 		.catch(err => {
-		// 			console.log(err);
-		// 			return err;
-		// 		});
-		// 	console.log('response');
-		// 	console.log(response);
-		// }
+		//!ALL the calls are working but the response doesn't wait for them...
+		console.log('response');
+		console.log(response);
+		console.table(response);
 
 		// console.log(response);
 		const normalized = normalize(response.leagues, [clubListEntity]);
