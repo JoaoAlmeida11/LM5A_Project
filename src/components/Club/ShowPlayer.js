ce607e7c8ea477e4b6caa3228c526c2e0178092d
João Almeida <joaoalmeida11@ua.pt>
2021-01-21 21:11:51 +0000

    Show like components coverage improv

--- src/components/Club/ShowPlayer.js
+++ src/components/Club/ShowPlayer.js
@@ -2,23 +2,21 @@ import { Link } from 'react-router-dom';
 import { Col, Button, Image } from 'react-bootstrap';
 
 // shows each player belonging to a club/team
-const ShowPlayer = ({ player, seasonId }) => {
-	return (
-		<Col xs={6} lg={3} className="text-center pb-4">
-			<div className=" playerCard pt-2">
-				<Image src={`${player.photo}`} alt={player.shortName} fluid />
-				<h3 className="mb-0 mt-2">{player.shortName}</h3>
-				<Button className="viewMoreButton mt-2 mb-2">
-					<Link
-						to={`/soccer/player/${seasonId}/${player.playerID}/`}
-						className="text-white text-decoration-none"
-					>
-						View more!
-					</Link>
-				</Button>
-			</div>
-		</Col>
-	);
-};
+const ShowPlayer = ({ player, seasonId }) => (
+	<Col xs={6} lg={3} className="text-center pb-4">
+		<div className=" playerCard pt-2">
+			<Image src={`${player.photo}`} alt={player.shortName} fluid />
+			<h3 className="mb-0 mt-2">{player.shortName}</h3>
+			<Button className="viewMoreButton mt-2 mb-2">
+				<Link
+					to={`/soccer/player/${seasonId}/${player.playerID}/`}
+					className="text-white text-decoration-none"
+				>
+					View more!
+				</Link>
+			</Button>
+		</div>
+	</Col>
+);
 
 export default ShowPlayer;
--- src/components/Homepage/ShowLeague.js
+++ src/components/Homepage/ShowLeague.js
@@ -1,27 +1,26 @@
 import { Link } from 'react-router-dom';
 import { Col, Button, Image } from 'react-bootstrap';
+const ShowLeague = ({ league }) => (
+	<Col xs={12} md={6} lg={4} className="text-center pt-4 ">
+		<div className="imgHeightLeague d-flex align-items-end justify-content-center">
+			<Image
+				className="imgConfig "
+				src={`${league.logo}`}
+				alt={'League of ' + league.name}
+				fluid
+			/>
+		</div>
+		<div className="pt-4">
+			<Button className="viewMoreButton">
+				<Link
+					to={`/soccer/league/${league.id}`}
+					className="text-white text-decoration-none"
+				>
+					View more!
+				</Link>
+			</Button>
+		</div>
+	</Col>
+);
 
-export default function ShowLeague({ league }) {
-	return (
-		<Col xs={12} md={6} lg={4} className="text-center pt-4 ">
-			<div className="imgHeightLeague d-flex align-items-end justify-content-center">
-				<Image
-					className="imgConfig "
-					src={`${league.logo}`}
-					alt={'League of ' + league.name}
-					fluid
-				/>
-			</div>
-			<div className="pt-4">
-				<Button className="viewMoreButton">
-					<Link
-						to={`/soccer/league/${league.id}`}
-						className="text-white text-decoration-none"
-					>
-						View more!
-					</Link>
-				</Button>
-			</div>
-		</Col>
-	);
-}
+export default ShowLeague;
