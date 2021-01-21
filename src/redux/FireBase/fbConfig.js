d9257c1967dfa4867d42204308bd5601f018e556
João Almeida <joaoalmeida11@ua.pt>
2021-01-21 04:49:28 +0000

    separated firebaseConfig from configureStore

--- src/redux/FireBase/fbConfig.js
+++ src/redux/FireBase/fbConfig.js
@@ -1,14 +1,21 @@
-// import firebase from 'firebase/app';
-// import 'firebase/auth';
+import firebase from 'firebase/app';
+import { store } from '../configureStore';
 
-// export const fbConfig = {
-// 	apiKey: process.env.FIREBASE_API_KEY,
-// 	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-// 	projectId: process.env.FIREBASE_PROJECT_ID,
-// 	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-// 	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-// 	appId: process.env.FIREBASE_APP_ID,
-// };
+const rrfConfig = {
+	userProfile: 'users',
+};
+export const rrfProps = {
+	firebase,
+	config: rrfConfig,
+	dispatch: store.dispatch,
+};
 
-// firebase.initializeApp(fbConfig);
-// export default firebase;
+export const fbConfig = {
+	apiKey: [process.env.REACT_APP_FIREBASE_API_KEY],
+	authDomain: [process.env.REACT_APP_FIREBASE_AUTH_DOMAIN],
+	projectId: [process.env.REACT_APP_FIREBASE_PROJECT_ID],
+	storageBucket: [process.env.REACT_APP_FIREBASE_STORAGE_BUCKET],
+	messagingSenderId: [process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID],
+	appId: [process.env.REACT_APP_FIREBASE_APP_ID],
+};
+firebase.initializeApp(fbConfig);
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -10,14 +10,13 @@ import {
 import { constants as rfConstants } from 'redux-firestore';
 
 // import firebase from './FireBase/fbConfig';
-import firebase from 'firebase/app';
 
 import 'firebase/database';
 import 'firebase/auth';
 
 const reducer = rootReducer;
 
-const store = configureStore({
+export const store = configureStore({
 	reducer,
 	middleware: getDefaultMiddleware({
 		serializableCheck: {
@@ -41,22 +40,3 @@ const store = configureStore({
 	devTools: process.env.NODE_ENV !== 'production',
 });
 export default store;
-
-const rrfConfig = {
-	userProfile: 'users',
-};
-export const rrfProps = {
-	firebase,
-	config: rrfConfig,
-	dispatch: store.dispatch,
-};
-
-export const fbConfig = {
-	apiKey: [process.env.REACT_APP_FIREBASE_API_KEY],
-	authDomain: [process.env.REACT_APP_FIREBASE_AUTH_DOMAIN],
-	projectId: [process.env.REACT_APP_FIREBASE_PROJECT_ID],
-	storageBucket: [process.env.REACT_APP_FIREBASE_STORAGE_BUCKET],
-	messagingSenderId: [process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID],
-	appId: [process.env.REACT_APP_FIREBASE_APP_ID],
-};
-firebase.initializeApp(fbConfig);
